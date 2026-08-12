d955afaa175aef43b2fcc2d327a8ec986a628765
Jennifer Ogbang <jenniferogbang@gmail.com>
2026-08-12 12:14:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// Promt and alert
+// alert("Hello Everyone");
+
+// let a = (prompt("Enter a number:"))
+// let b = (prompt("Enter a number:"))
+
+// let a = parseInt(prompt("Enter a number:"))
+// let b = parseInt(prompt("Enter a number:"))
+
+// alert (a+b)
+// Promt and alert
+
+
+// CONDITIONALS
+
+// if statement
+// if (5 > 7) {
+//     console.log("5 is greater than 7")
+// }
+
+
+// if (5 > 7) {
+//     console.log("5 is greater than 7")
+// } else {
+//     console.log("7 is greater than 5")
+// }
+
+
+// let num = 5;
+// if (num > 5) {
+//     console.log("round to 1")
+// } else if (num < 5){
+//     console.log("round to 0")
+// } else if (num == 5){
+//     console.log("round to 5")
+// }
+
+
+// let systemName = prompt("who are you?").toLowerCase();
+// if(systemName === "jennifer" || systemName === "mariam") {
+//     alert("Welcome to the system")
+// } else {
+//     alert("You are not allowed to enter the system");
+// }
+
+
+let useraccess = prompt ("what are you wearing?");
+if (useraccess === "hat") {
+    alert("Welcome to the system");
+} else if (useraccess === "glasses") {
+    alert("you can enter with your glasses");
+} else if (useraccess === "mask") {
+    alert("you can enter with your mask");
+} else {
+    alert("you are not allowed to enter the system");
+}
+
+
+// TEMPLATE LITERALS
+// let name = "Alice";
+// let age = 25;
+// console.log(`My name is ${name} and I am ${age} years old.`);
